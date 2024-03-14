f5f4178de83b3d32fce50326fb7edb38ea12b9aa
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-03-14 02:27:44 +0000

    arrange the background and add the youtube video

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		 <nav className="navbar navbar-dark bg-dark"> 
+		 <nav className="navbar navbar-dark" style= {{background: "rgba(33, 33, 33, 1)" }}> 
 			<div className="container-fluid">
 				<div className="Navbar-image">
 					<p className="text-light">Tekken8</p>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,18 +3,30 @@ import { Context } from "../store/appContext";
 // import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import tekkenBackgroundUrl from "../../img/tekken8-background.png";
+import Card from "../component/cards.jsx";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<>
-		<div className="Background-Body">
-			<img src={tekkenBackgroundUrl} className="background-image"></img>
-		</div>
-		<div className="Cards">
-		</div>
-		</>
+				<div className="Body-Cards" style= {{backgroundImage: `url(${tekkenBackgroundUrl})` }}>
+					<div className="Cards">
+					{/* (<Card />) */}
+					<h1 className="text-light text-center p-5">HELLO WORLD</h1>
+				</div>
+				</div>
+
+				<div className="Body-trailer" style= {{background: "rgba(33, 33, 33, 1)" }}>
+					<iframe  src="https://www.youtube.com/watch?v=ToKJfywbe1o&pp=ygUbdHJhaWxlciBkZSB0ZWtrZW4gOCBvZmljaWFs" className="align-items-center m-5"></iframe>
+				</div>
+
+				<div className="Recomendations-Body" style= {{backgroundImage: `url(${tekkenBackgroundUrl})` }}>
+					
+					<h1 className="text-light text-center p-5">Recomendaciones</h1>
+				</div>
 		
+		</>
+
 	);
 };
