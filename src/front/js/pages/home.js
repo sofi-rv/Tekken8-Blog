752e32f4677711c8791173eeaca7e4edac96c286
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-03-21 01:42:35 +0000

    created the Cards-recomendations

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -11,22 +11,48 @@ export const Home = () => {
 	return (
 		<>
 			<div className="Body-Cards" style={{ backgroundImage: `url(${tekkenBackgroundUrl})` }}>
-				<div className="Cards p-5 d-flex">
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-	
+				<div className="container Cards">
+					<div className="Colum1 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum2 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum3 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum4 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum5 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum6 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum7 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+					<div className="Colum8 d-flex">
+						<Card />, <Card />, <Card />, <Card />
+					</div>
+
 				</div>
 			</div>
 
 			<div className="Body-trailer" style={{ background: "rgba(33, 33, 33, 1)" }}>
-				<iframe src="https://www.youtube.com/embed/ToKJfywbe1o" className="align-items-center m-5"></iframe>
+				<iframe src="https://www.youtube.com/embed/ToKJfywbe1o" className="m-5" style={{ width: "500px", height: "260px" }}></iframe>
 			</div>
 
 			<div className="Recomendations-Body" style={{ backgroundImage: `url(${tekkenBackgroundUrl})` }}>
 
-				<h1 className="text-light text-center p-5">Recomendaciones</h1>
+				<div>
+					<h1 className="text-light container p-5 Recomendations">Recomendaciones</h1>
+				</div>
+				<div className="Articles container pb-5 d-flex"> 
+				<Card />, <Card />, <Card />, <Card />, <Card />
+				</div>
+
 			</div>
 
 		</>
