a14afad547179665202937e4f099c6982ab6e605
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-04-23 22:32:22 +0000

    changes

--- src/front/js/component/cards.jsx
+++ src/front/js/component/cards.jsx
@@ -8,7 +8,10 @@ const Card = () =>{
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="button-cards">
+    {/* <Link to="/detailCards" className="btn btn-outline-primary">Learn More!</Link> */}
+     <a href="#" className="btn btn-primary">Go somewhere</a> 
+    </div>
   </div>
 </div>
 )
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -31,7 +31,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         {/* <Route element={<Single />} path="/single/:theid" /> */}
                         <Route element={<h1>Not found!</h1>} />
-                        <Route element={<DetailCards />} path="/details" />
+                        <Route element={<DetailCards />} path="/detailCards" />
 
                     </Routes>
                     <Footer />
