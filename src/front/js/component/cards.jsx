ad89ba4f636d953bc5df0966fbf5bab2458e1dc0
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-05-13 17:51:31 +0000

    Adjust the border-card

--- src/front/js/component/cards.jsx
+++ src/front/js/component/cards.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Card = (props) =>{
+const Card = (props) => {
   return (
-  
-<div className="card m-4" style= {{width: "18rem" }}>
-  <img src= {props.imagen} className="card-img-top" alt="..."></img>
-  <div className="card-body">
-    <h5 className="card-title">{props.titulo}</h5>
-    <p className="card-text">{props.descripcion}</p>
-    <div className="button-cards">
-    <Link to="/detailCards"><button className="btn btn-primary">Go somewhere</button> </Link>
-     
+
+    <div className="card m-4 border-0" style={{ width: "18rem" }}>
+      <img src={props.imagen} className="card-img-top" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">{props.titulo}</h5>
+        <div className="button-cards">
+          <Link to="/detailCards"><button className="btn btn-primary">Go somewhere</button> </Link>
+          
+
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-)
+  )
 }
 
 export default Card; 
