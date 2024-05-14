60d07b12ab4325346b850a155b42a2b096da1bd8
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-05-14 20:56:13 +0000

    adjust the cards

--- src/front/js/component/charactersCard.jsx
+++ src/front/js/component/charactersCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/cards.css"
-const Card = (props) => {
+const CharactersCard = (props) => {
   return (
-<div className="row">
-    <div className="card m-4 border-0 col-md-3" style={{ width: "18rem" }}>
+
+    <div className="card m-4 p-0 border-0 col-md-3" style={{ width: "18rem" }}>
       <img src={props.imagen} className="card-img-top" alt="..."></img>
       <div className="card-body">
         <h5 className="card-title">{props.titulo}</h5>
@@ -13,8 +13,8 @@ const Card = (props) => {
         </div>
       </div>
     </div>
-    </div>
+    
   )
 }
 
-export default Card; 
+export default CharactersCard; 
