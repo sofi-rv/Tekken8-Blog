4afd874eb0d6879858701454c5e76e8b031337b4
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-05-14 21:45:15 +0000

    changes

--- src/front/js/component/charactersCard.jsx
+++ src/front/js/component/charactersCard.jsx
@@ -4,7 +4,7 @@ import "../../styles/cards.css"
 const CharactersCard = (props) => {
   return (
 
-    <div className="card m-4 p-0 border-0 col-md-3" style={{ width: "18rem" }}>
+    <div className="Card-Hover card m-4 p-0 border-0 col-md-3" style={{ width: "18rem" }}>
       <img src={props.imagen} className="card-img-top" alt="..."></img>
       <div className="card-body">
         <h5 className="card-title">{props.titulo}</h5>
