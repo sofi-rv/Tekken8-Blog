c1e049ee390b39331afe838a1a5ceaf20aa12ef3
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-05-15 20:59:34 +0000

    adjust the button-cards

--- src/front/js/component/charactersCard.jsx
+++ src/front/js/component/charactersCard.jsx
@@ -4,13 +4,11 @@ import "../../styles/cards.css"
 const CharactersCard = (props) => {
   return (
 
-    <div className="Card-Hover card m-4 p-0 border-0 col-md-3" style={{ width: "18rem" }}>
-      <img src={props.imagen} className="card-img-top image" style={{ width: "18rem", height: "26rem" }} alt="..."></img>
-      {/* <div className="card-body" style={{ height: "0rem" }}> */}
-        <div className="button-cards middle">
-          <Link to="/detailCards"><button className="btn btn-primary text">{props.titulo}</button> </Link>
+    <div className="card m-4 p-0 border-0 col-md-3" style={{ width: "18rem" }}>
+      <img src={props.imagen} className="card-img-top image" style={{ width: "18rem", height: "25rem" }} alt="..."></img>
+        <div className="button-cards">
+          <Link to="/detailCards"><button className="btn btn-danger border-0 text">{props.titulo}</button> </Link>
         </div>
-      {/* </div> */}
     </div>
     
   )
