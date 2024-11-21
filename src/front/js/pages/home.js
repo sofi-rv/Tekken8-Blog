b9784c727e22b1344af342b0b7159432becb75a1
Sofía Ramírez <128938425+sofi-rv@users.noreply.github.com>
2024-11-21 05:52:58 +0000

    se agrego fondo de tekken8

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import tekken8ImageUrl from "../../img/Tekken8-fondo.png";
 import "../../styles/home.css";
 
 export const Home = () => {
